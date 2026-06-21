992c25f94780e45cba6be2747007736b0ee7abbe
Direk Kakkar <millioniar1717@gmail.com>
2026-06-22 02:51:38 +0530

    tests

--- PHASE01/C_MatrixTransformationsAndEigenValues/tests/test_eigenValues.py
+++ PHASE01/C_MatrixTransformationsAndEigenValues/tests/test_eigenValues.py
@@ -3,6 +3,8 @@ import os
 import pytest 
 import numpy as np
 sys.path.append(r"E:\ML\PHASE01\C_MatrixTransformationsAndEigenValues\core")
+from eigenValues import eigenvalues_2x2, eigenvector_2x
+
 
 # --------------
 # Eigen values
--- PHASE01/C_MatrixTransformationsAndEigenValues/tests/test_numpyRoutes.py
+++ PHASE01/C_MatrixTransformationsAndEigenValues/tests/test_numpyRoutes.py
@@ -3,6 +3,8 @@ import os
 import pytest 
 import numpy as np
 sys.path.append(r"E:\ML\PHASE01\C_MatrixTransformationsAndEigenValues\core")
+from numpyRoutes import rotation_3d_z, rotation_3d_x
+
 
 # -----------------
 # Roataion 3D `z`
