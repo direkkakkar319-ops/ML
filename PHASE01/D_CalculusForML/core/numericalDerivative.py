a9bcb5bb3bf60b9226047d4ad2d7b669fb40f533
Direk Kakkar <millioniar1717@gmail.com>
2026-06-21 23:19:39 +0530

    numericalDerivatie

---
+++ PHASE01/D_CalculusForML/core/numericalDerivative.py
@@ -0,0 +1,17 @@
+"""
+Numerical Derivative 
+Analytical Derivative
+"""
+def numirical_derivative(f, x, h=1e-7):
+    return (f(x + h) - f(x - h)) / (2 * h)
+
+
+def f(x):
+    return x ** 2
+
+
+nums = [-2, -1, 0, 1, 2]
+for num in nums:
+    numerical = numirical_derivative(f, num)
+    analytical = 2 * num 
+    print(f"x={num:2d} f'(x) numerical={numerical:.6f}  analytical={analytical:.1f}")
