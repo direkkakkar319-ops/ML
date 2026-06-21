14c54d0f22b503930e0f394f24c3d76d7699ef11
Direk Kakkar <millioniar1717@gmail.com>
2026-06-22 02:48:07 +0530

    tests

---
+++ PHASE01/C_MatrixTransformationsAndEigenValues/tests/test_eigenValues.py
@@ -0,0 +1,14 @@
+import sys
+import os
+import pytest 
+import numpy as np
+sys.path.append(r"E:\ML\PHASE01\C_MatrixTransformationsAndEigenValues\core")
+
+# --------------
+# Eigen values
+# --------------
+
+
+# ----------------
+# Eigen Vectors
+# ----------------
---
+++ PHASE01/C_MatrixTransformationsAndEigenValues/tests/test_numpyRoutes.py
@@ -0,0 +1,14 @@
+import sys
+import os
+import pytest 
+import numpy as np
+sys.path.append(r"E:\ML\PHASE01\C_MatrixTransformationsAndEigenValues\core")
+
+# -----------------
+# Roataion 3D `z`
+# -----------------
+
+
+# -----------------
+# Roataion 3D `x`
+# -----------------
