ba233ca524ee6649f8ddff5d2432968b75e7c85f
Direk Kakkar <millioniar1717@gmail.com>
2026-06-27 00:31:17 +0530

    test tanh

--- PHASE01/E_ChainRuleAndAutomaticDifferentiation/test/core/test_value.py
+++ PHASE01/E_ChainRuleAndAutomaticDifferentiation/test/core/test_value.py
@@ -1,29 +1,30 @@
 import pytest
 import sys
+import math
 
 sys.path.append(r"E:\ML\PHASE01\E_ChainRuleAndAutomaticDifferentiation\core")
 from value import Value
 
 
 @pytest.fixture()
-def function_is_two():
-    x = Value(2)
+def power_function():
+    x = Value(data=2)
     y = x ** 3
     return x,y
 
 
-def test_value(function_is_two):
-    x, y = function_is_two
+def test_value(power_function):
+    x, y = power_function
 
     assert y.data == 8
 
 
-def test_pow(function_is_two):
+def test_pow(power_function):
     """
     Question
         Verify that d/dx(x^3) at x=2 equals 12.0.
     """
-    x, y = function_is_two
+    x, y = power_function
     y.backward()
 
     assert x.grad == 12
@@ -34,3 +35,12 @@ def test_tanh():
     Question
         Verify that tanh'(0) = 1 and tanh'(2) = 0.0707 (approx).
     """
+    x = Value(data=2.0)
+    y = x.tanh()
+
+    assert y.data == pytest.approx(math.tanh(2.0))
+
+    x = Value(data=0.0)
+    y = x.tanh()
+
+    assert y.data == pytest.approx(math.tanh(0))
